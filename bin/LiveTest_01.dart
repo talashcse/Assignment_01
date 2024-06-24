abee6ffbdc30db40fdff2cf95b6a0a5597edb5cf
Talash <talash.cse@gmail.com>
2024-06-24 23:21:40 +0600

    Updated

--- bin/LiveTest_01.dart
+++ bin/LiveTest_01.dart
@@ -1,5 +1,27 @@
-void main(){
 
-  print('hello World');
+class Media {
+  void play() {
+    print('Playing media...');
+  }
+}
 
+class Song extends Media {
+late  String artist;
+  Song(this.artist);
+
+  @override
+  void play() {
+    print('Playing song by $artist...');
+  }
+}
+
+void main() {
+  Media media = Media();
+  Song song = Song('Imagination of World');
+
+  print('Calling play() on Media instance:');
+  media.play();
+
+  print('\n Calling play() on Song instance:');
+  song.play();
 }
